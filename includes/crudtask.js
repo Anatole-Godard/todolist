a1a786cb7e346c04c80921f2e1a62d52d8947a50
GODARD Anatole <anatole.godard@icloud.com>
2020-03-31 17:19:56 +0200

    Add lastTask number

--- includes/crudtask.js
+++ includes/crudtask.js
@@ -4,26 +4,39 @@ $(document).ready(function () {
 
     var db = firebase.firestore();
 
-    console.log(db);
+    db.collection("user").doc(idUser).get().then(function (doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.data().nbTask + 1);
+        return lastTask = doc.data().nbTask + 1;
+    });
 
-    $("#create").click(function() {
+    $("#create").click(function () {
+
+
         let nameTask = $("#nametask").val();
         let descriptiontask = $("#descriptiontask").val();
         let datetask = $("#datetask").val();
         let dateremindertask = $("#dateremindertask").val();
         let categorytask = $("#category").val();
 
-        db.collection("user").doc(idUser).collection('tasks').doc('1').set({
+        db.collection("user").doc(idUser).collection('tasks').doc(lastTask.toString()).set({
             name: nameTask,
             description: descriptiontask,
             date: datetask,
             datereminder: dateremindertask,
             category: categorytask,
         })
-            .then(function(docRef) {
-                console.log("Document written with ID: ", docRef.id);
+            .then(function (docRef) {
+                db.collection("user").doc(idUser).set({
+                    nbTask: lastTask
+                }).then(function (docRef) {
+                    console.log(docRef)
+                })
+                    .catch(function (error) {
+                        console.error("Error adding document: ", error);
+                    });
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.error("Error adding document: ", error);
             });
 
