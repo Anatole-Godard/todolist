fb764589f7e1a4b4a62791fcb93fbee34ea1c654
Beatrice BRIAND <bbriand1@gmail.com>
2020-03-31 19:31:47 +0200

    ajout du read

--- includes/crudtask.js
+++ includes/crudtask.js
@@ -40,6 +40,62 @@ $(document).ready(function () {
                 console.error("Error adding document: ", error);
             });
 
-    })
+    });
+
+
+
+
+
+
+
+    $("#read").click(function() {
+
+        db.collection("user").doc(idUser).collection('tasks').get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                //   var taskDateReminder = doc.data().datereminder ;
+                var ligne = '<tr>' +
+                    '<td>'+doc.id+'</td>' +
+                    '<td>'+doc.data().name +'</td>' +
+                    '<td>'+doc.data().category+'</td>' +
+                    '<td>'+doc.data().date+'</td>' +
+                    '<td>'+doc.data().datereminder+'</td>' +
+                    '<td>'+doc.data().description+'</td>' +
+                    '<td><button class="btn btn-default bg-danger rounded text-light" id="btnUpdate">Modifier</button></td>' +
+                    '<td><button class="btn btn-default bg-danger rounded text-light" id="delete">Supprimer</button></td>' +
+                    '</tr>' ;
+                $('#myTableTaskToDo > tbody:last-child').append(ligne);
+                // console.log(`${doc.id} => ${doc.data().category}`);
+            });
+        });
+    });
+
+
+
+    ;
+    $("#update").click(function() {
+        db.collection("user").doc(idUser).collection('tasks').get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+
+                var taskID = doc.id;
+                var taskName = doc.data().name ;
+                var taskCategory = doc.data().category;
+                var taskDateEcheance = doc.data().date;
+                //   var taskDateReminder = doc.data().datereminder ;
+                var taskDescription = doc.data().description ;
+                var ligne = '<tr>' +
+                    '<td>'+taskID+'</td>' +
+                    '<td>'+taskName+'</td>' +
+                    '<td>'+taskCategory+'</td>' +
+                    '<td>'+taskDateEcheance+'</td>' +
+                    '<td>'+doc.data().datereminder+'</td>' +
+                    '<td>'+taskDescription+'</td>' +
+                    '<td><button class="btn btn-default bg-danger rounded text-light" id="update" >Modifier</button></td>' +
+                    '<td><button class="btn btn-default bg-danger rounded text-light" id="delete" >Supprimer</button></td>' +
+                    '</tr>' ;
+                $('#myTableTaskToDo > tbody:last-child').append(ligne);
+                // console.log(`${doc.id} => ${doc.data().category}`);
+            });
+        });
+    });
 
 });
